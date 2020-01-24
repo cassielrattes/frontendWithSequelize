c03518eb1b9c46993d65044ee363ee37082ed1ff
Cassiel Rattes Cortez <crcortez@fiecalunos.lan>
2020-01-24 16:34:17 -0300

    ...

--- src/pages/layouts/Navbar/index.js
+++ src/pages/layouts/Navbar/index.js
@@ -1,52 +1,62 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import "./styles.css";
+import { isAuthenticated, logout } from "../../../services/auth";
 
 const Navbar = () => {
+  const authLinks = (
+    <ul className="nav navbar-nav ml-auto">
+      <li className="nav-item" role="presentation">
+        <Link className="nav-link" to="/dashboard">
+          <i className="fas fa-user"></i>{" "}
+          <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+      <li className="nav-item" role="presentation">
+        <a className="nav-link" onClick={logout} href="#!">
+          <i className="fas fa-sign-out-alt"></i>{" "}
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </ul>
+  );
+
+  const guestLinks = (
+    <span className="navbar-text actions ml-auto">
+      <Link to="/login" className="login">
+        Sign In
+      </Link>
+      <Link
+        className="btn btn-light action-button"
+        role="button"
+        to="/register"
+      >
+        Sign Up
+      </Link>
+    </span>
+  );
+
   return (
-    <div>
-      <nav className="navbar navbar-light navbar-expand-md navigation-clean-button">
-        <div className="container">
+    <nav className="navbar navbar-light navbar-expand-md navigation-clean-button">
+      <div className="container">
+        <h1>
           <Link className="navbar-brand" to="/">
-            Gustavo's Company
+            <i className="fas fa-code"></i> Gustavo's Company
           </Link>
-          <button
-            className="navbar-toggler"
-            data-toggle="collapse"
-            data-target="#navcol-1"
-          >
-            <span className="sr-only">Toggle navigation</span>
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navcol-1">
-            <ul className="nav navbar-nav mr-auto">
-              <li className="nav-item" role="presentation">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item" role="presentation">
-                <Link className="nav-link" to="/dashboard">
-                  Dashboard
-                </Link>
-              </li>
-            </ul>
-            <span className="navbar-text actions">
-              <Link to="/login" className="login">
-                Sign In
-              </Link>
-              <Link
-                className="btn btn-light action-button"
-                role="button"
-                to="/register"
-              >
-                Sign Up
-              </Link>
-            </span>
-          </div>
+        </h1>
+        <button
+          className="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#navcol-1"
+        >
+          <span className="sr-only">Toggle navigation</span>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navcol-1">
+          {<Fragment>{isAuthenticated() ? authLinks : guestLinks}</Fragment>}
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
